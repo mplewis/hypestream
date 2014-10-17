cd7dc9e0265db4b2df7f804e436b1b92d8369ec3
Matthew Lewis <matt@mplewis.com>
2014-10-17 14:36:23 -0500

    Initial Commit

---
+++ Hypestream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hypestream
+//
+//  Created by Matthew Lewis on 10/17/14.
+//  Copyright (c) 2014 Kestrel Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
