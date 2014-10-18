e6355df78fd0a48c90b7de9023a0462a4301cf14
Matthew Lewis <matt@mplewis.com>
2014-10-18 16:36:04 -0500

    Progress: retrieves HypeM tracks from web service

--- Hypestream/ViewController.swift
+++ Hypestream/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    @IBOutlet weak var outputView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func refreshFeed(AnyObject) {
+        outputView.text = "Loading..."
+        let url = NSURL(string: "http://hypem.com/playlist/popular/3day/json/1/data.js")
+        let request = NSURLRequest(URL: url)
+        let queue = NSOperationQueue.mainQueue()
+        let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue) {(response, jsonData, error) in
+            let cookies = cookieStorage.cookiesForURL(url)
+            println("Cookies: \(cookies)")
+
+            var err: NSError?
+            let data: AnyObject? = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: &err)
+            if (data == nil) {
+                println("No data received")
+                return
+            }
+            if (err != nil) {
+                println("JSON error: \(err!.localizedDescription)")
+                return
+            }
+            if let tracks = data as? Dictionary<String, AnyObject> {
+                for index in 0...9 {
+                    if let track: AnyObject = tracks[String(index)] {
+                        println("\(track)")
+                    } else {
+                        println("Couldn't get track from String(\(index))")
+                    }
+                }
+            } else {
+                println("Couldn't cast data to Dictionary<Int, AnyObject>")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
