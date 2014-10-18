5989f42af87e69fc50c9686678d262c7e18d7ee5
Matthew Lewis <matt@mplewis.com>
2014-10-18 18:53:36 -0500

    Clean up code a bit and reduce nesting

--- Hypestream/ViewController.swift
+++ Hypestream/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var outputView: UITextView!
@@ -19,9 +18,7 @@ class ViewController: UIViewController {
         let queue = NSOperationQueue.mainQueue()
         let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
         NSURLConnection.sendAsynchronousRequest(request, queue: queue) {(response, jsonData, error) in
-            let cookies = cookieStorage.cookiesForURL(url)
-            println("Cookies: \(cookies)")
-
+            // Parse JSON data
             var err: NSError?
             let data: AnyObject? = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: &err)
             if (data == nil) {
@@ -32,33 +29,50 @@ class ViewController: UIViewController {
                 println("JSON error: \(err!.localizedDescription)")
                 return
             }
-            if let tracks = data as? Dictionary<String, AnyObject> {
-                var maxIndex = 0;
-                for key in tracks.keys {
-                    let keyInt = key.toInt()
-                    if (keyInt != nil && keyInt > maxIndex) {
-                            maxIndex = keyInt!
-                    }
-                }
-                var output = ""
-                for index in 0...maxIndex {
-                    if let track: AnyObject = tracks[String(index)] {
-                        let artist = track["artist"]
-                        let title = track["title"]
-                        if (artist != nil && title != nil) {
-                            let a: AnyObject? = artist
-                            let t: AnyObject? = title
-                            output += "\(index): \(a!) - \(t!)\n"
-                        } else {
-                            println("Couldn't get artist or title for \(index)")
-                        }
-                    } else {
-                        println("Couldn't get track from String(\(index))")
-                    }
-                }
-                self.outputView.text = output
+            let tracksMaybe =  data as? Dictionary<String, AnyObject>;
+            if (tracksMaybe != nil) {
+                println("Successfully retrieved tracks")
             } else {
-                println("Couldn't cast data to Dictionary<Int, AnyObject>")
+                println("Couldn't retrieve tracks")
+                return
+            }
+            let tracks = tracksMaybe!
+
+            // Get track count
+            var maxIndex = 0;
+            for key in tracks.keys {
+                let keyInt = key.toInt()
+                if (keyInt != nil && keyInt > maxIndex) {
+                    maxIndex = keyInt!
+                }
+            }
+            
+            // Validate all tracks
+            var validTracks = [AnyObject]()
+            for index in 0...maxIndex {
+                if let track: AnyObject = tracks[String(index)] {
+                    validTracks.append(track)
+                }
+            }
+            
+            // Print track titles to screen
+            var output = ""
+            for track in validTracks {
+                let artist = track["artist"]
+                let title = track["title"]
+                if (artist != nil && title != nil) {
+                    let a: AnyObject? = artist
+                    let t: AnyObject? = title
+                    output += "\(a!) - \(t!)\n"
+                } else {
+                    println("Couldn't get artist or title for \(index)")
+                }
+            }
+            self.outputView.text = output
+            
+            // Get an mp3 url for each track
+            for track in validTracks {
+                // TODO
             }
         }
     }
