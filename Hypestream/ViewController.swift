2d56803f692982107bb22ad04972f65e73c31a4b
Matthew Lewis <matt@mplewis.com>
2014-10-19 14:22:40 -0500

    Add json.swift; refactor json processing code to match

--- Hypestream/ViewController.swift
+++ Hypestream/ViewController.swift
@@ -18,46 +18,32 @@ class ViewController: UIViewController {
 
     @IBAction func refreshFeed(AnyObject) {
         outputView.text = "Loading..."
-        let url = NSURL(string: "http://hypem.com/playlist/popular/3day/json/1/data.js")
-        let request = NSURLRequest(URL: url)
         let queue = NSOperationQueue.mainQueue()
-        let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
-        NSURLConnection.sendAsynchronousRequest(request, queue: queue) {(response, jsonData, error) in
-            // Parse JSON data
-            var err: NSError?
-            let data: AnyObject? = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: &err)
-            if (data == nil) {
-                println("No data received")
-                return
-            }
-            if (err != nil) {
-                println("JSON error: \(err!.localizedDescription)")
-                return
-            }
-            let tracksMaybe =  data as? Dictionary<String, AnyObject>;
-            if (tracksMaybe != nil) {
-                println("Successfully retrieved tracks")
-            } else {
-                println("Couldn't retrieve tracks")
-                return
-            }
-            let tracks = tracksMaybe!
+        let feedUrl = NSURL(string: "http://hypem.com/playlist/popular/3day/json/1/data.js")
+        let request = NSURLRequest(URL: feedUrl)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response, jsonData, error) in
+            let httpResponse = response as NSHTTPURLResponse
 
+            // Parse JSON data
+            let tracks = JSON(string: NSString(data: jsonData, encoding: NSUTF8StringEncoding))
+            
             // Get track count
             var maxIndex = 0;
-            for key in tracks.keys {
-                let keyInt = key.toInt()
-                if (keyInt != nil && keyInt > maxIndex) {
-                    maxIndex = keyInt!
+            for (key, value) in tracks {
+                // HypeM track numbers are strings. Try to cast them to ints.
+                if let k = (key as String).toInt() {
+                    if k > maxIndex {
+                        maxIndex = k
+                    }
                 }
             }
             
             // Validate all tracks
-            var validTracks = [AnyObject]()
+            var validTracks = [JSON]()
             for index in 0...maxIndex {
-                if let track: AnyObject = tracks[String(index)] {
-                    validTracks.append(track)
-                }
+                let indexString = String(index)
+                validTracks.append(tracks[indexString])
             }
             
             // Print track titles to screen
@@ -65,13 +51,7 @@ class ViewController: UIViewController {
             for track in validTracks {
                 let artist = track["artist"]
                 let title = track["title"]
-                if (artist != nil && title != nil) {
-                    let a: AnyObject? = artist
-                    let t: AnyObject? = title
-                    output += "\(a!) - \(t!)\n"
-                } else {
-                    println("Couldn't get artist or title for \(index)")
-                }
+                output += "\(artist) - \(title)\n"
             }
             self.outputView.text = output
             
