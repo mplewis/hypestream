262df2e0964962a81b4c09fe1fddb4816ae218b3
Matthew Lewis <matt@mplewis.com>
2014-10-20 00:28:27 -0500

    Download tracks to Documents on click

--- Hypestream/ViewController.swift
+++ Hypestream/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+let queue = NSOperationQueue.mainQueue()
+let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
 
@@ -37,45 +40,70 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let track = tracks[indexPath.row]
+        let id = track["id"]
+        let key = track["key"]
+        let artist = track["artist"]
+        let title = track["song"]
         
+        let mediaUrl = NSURL(string: "http://hypem.com/serve/source/\(id)/\(key)")
+        let mediaRequest = NSMutableURLRequest(URL: mediaUrl)
+        mediaRequest.HTTPMethod = "POST"
+        
+        NSURLConnection.sendAsynchronousRequest(mediaRequest, queue: queue) { (response, jsonData, error) in
+            if (jsonData == nil) {
+                println("Couldn't retrieve stream URL for \(artist) - \(title)")
+                return
+            }
+            let jsonString: String = NSString(data: jsonData, encoding: NSUTF8StringEncoding)
+            let jsonData = JSON(string: jsonString)
+            if let streamUrlString = jsonData["url"].asString {
+                println("Retrieving track from \(streamUrlString)")
+
+                var error: NSError?
+                let streamUrl = NSURL(string: streamUrlString)
+                let trackData = NSData.dataWithContentsOfURL(streamUrl, options: nil, error: &error)
+                if (error != nil) {
+                    println(error!.localizedDescription)
+                    return
+                }
+                
+                let targetPath: String = NSString.pathWithComponents([documentsPath, "\(artist) - \(title).mp3"])
+                let writeSuccess = trackData.writeToFile(targetPath, atomically: true)
+                if (writeSuccess) {
+                    println("Write successful for \(targetPath)")
+                } else {
+                    println("Write failed for \(targetPath)")
+                }
+            } else {
+                println("Couldn't get stream URL for \(artist) - \(title)")
+                return
+            }
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
     @IBAction func refreshFeed(AnyObject) {
-        let queue = NSOperationQueue.mainQueue()
         let homeUrl = NSURL(string: "http://hypem.com/popular/1")
         
         NSURLConnection.sendAsynchronousRequest(NSURLRequest(URL: homeUrl), queue: queue) { (response, htmlData, error) in
+            if (htmlData == nil) {
+                println("Couldn't scrape HTML from Hype Machine")
+                return
+            }
             let htmlString = NSString(data: htmlData, encoding: NSUTF8StringEncoding)
             let startScript = "<script type=\"application/json\" id=\"displayList-data\">"
             let endScript = "</script>"
             if let partial = htmlString.componentsSeparatedByString(startScript)[1] as? String {
                 let script = partial.componentsSeparatedByString(endScript)[0]
                 let scriptJson = JSON(string: script)
-                let tracks = scriptJson["tracks"].asArray
-                if (tracks == nil) {
+                let retrieved = scriptJson["tracks"].asArray
+                if (retrieved == nil) {
                     println("Couldn't load tracks")
                     return
                 }
 
-                self.tracks = tracks!
-
-                for track in tracks! {
-                    let id = track["id"]
-                    let key = track["key"]
-                    let artist = track["artist"]
-                    let title = track["song"]
-
-                    let mediaUrl = NSURL(string: "hypem.com/serve/source/\(id)/\(key)")
-                    let mediaRequest = NSMutableURLRequest(URL: mediaUrl)
-                    mediaRequest.HTTPMethod = "POST"
-                    
-                    NSURLConnection.sendAsynchronousRequest(mediaRequest, queue: queue) { (response, jsonData, error) in
-                        let jsonString: String = NSString(data: jsonData, encoding: NSUTF8StringEncoding)
-                        let jsonData = JSON(string: jsonString)
-                        let streamUrl = jsonData["url"]
-                        println("\(artist) - \(title): \(streamUrl)")
-                    }
-                }
+                self.tracks = retrieved!
             } else {
                 println("Failure on splitting start")
             }
