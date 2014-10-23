3f0f83ee82a05807f432e0948f270df389c89e97
Matthew Lewis <matt@mplewis.com>
2014-10-23 17:19:52 -0500

    Start linking labels and reactive variables

--- Hypestream/Controllers/HypeTrackCell.swift
+++ Hypestream/Controllers/HypeTrackCell.swift
@@ -9,18 +9,31 @@
 import UIKit
 
 class HypeTrackCell: UITableViewCell {
-    var artist: String
-    var title: String
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    var artist: String {
+        set {
+            self.artist = newValue
+            self.artistLabel.text = newValue
+        }
+        get {
+            return self.artist
+        }
+    }
+    var title: String {
+        set {
+            self.title = newValue
+            self.titleLabel.text = newValue
+        }
+        get {
+            return self.title
+        }
+    }
     
     required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         self.artist = ""
         self.title = ""
-        super.init(coder: aDecoder)
-    }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
