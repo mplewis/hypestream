ed71eb3a51bb4f39eaa28db9c061d8b2b5b470c5
Matthew Lewis <matt@mplewis.com>
2014-11-06 19:58:48 -0600

    Add new ViewControllers for each tab

---
+++ Hypestream/Controllers/InboxViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InboxViewController.swift
+//  Hypestream
+//
+//  Created by Matthew Lewis on 11/6/14.
+//  Copyright (c) 2014 Kestrel Development. All rights reserved.
+//
+
+import UIKit
+
+class InboxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
